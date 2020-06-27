f467a06222200bafe8132f18d3f6c73d81f9fe5c
Shawn Stolsig <shawnstolsig@gmail.com>
2020-06-27 15:34:48 -0700

    added babel plugin loader/config

---
+++ .babelrc.js
@@ -0,0 +1,26 @@
+const plugins = [
+    // only import the components from '@material-ui/core' that are specifically named imports in {}
+    [
+      'babel-plugin-import',
+      {
+        'libraryName': '@material-ui/core',
+        // Use "'libraryDirectory': ''," if your bundler does not support ES modules
+        'libraryDirectory': 'esm',
+        'camel2DashComponentName': false
+      },
+      'core'
+    ],
+    // only import the components from '@material-ui/icons' that are specifically named imports in {}
+    [
+      'babel-plugin-import',
+      {
+        'libraryName': '@material-ui/icons',
+        // Use "'libraryDirectory': ''," if your bundler does not support ES modules
+        'libraryDirectory': 'esm',
+        'camel2DashComponentName': false
+      },
+      'icons'
+    ]
+  ];
+  
+  module.exports = {plugins};
